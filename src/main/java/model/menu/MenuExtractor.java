6e7d8d93ccb0b404ee1ae97d1b5313f290263438
Paky29 <p.somma11@studenti.unisa.it>
2021-05-15 00:03:33 +0200

    modificato ok

--- src/main/java/model/menu/MenuExtractor.java
+++ src/main/java/model/menu/MenuExtractor.java
@@ -1,4 +1,16 @@
 package model.menu;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class MenuExtractor {
+    public static Menu extract(ResultSet set) throws SQLException {
+        Menu m=new Menu();
+        m.setCodice(set.getInt("m.codiceMenu"));
+        m.setNome(set.getString("m.nome"));
+        m.setPrezzo(set.getFloat("m.prezzo"));
+        m.setSconto(set.getInt("m.sconto"));
+        m.setValido(set.getBoolean("m.valido"));
+        return m;
+    }
 }
