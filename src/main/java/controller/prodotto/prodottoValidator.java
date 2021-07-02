36cc5c76ce42b660d390b9a10ae87f3b65552291
Paky29 <p.somma11@studenti.unisa.it>
2021-07-02 11:39:48 +0200

    modificato ok

--- src/main/java/controller/prodotto/prodottoValidator.java
+++ src/main/java/controller/prodotto/prodottoValidator.java
@@ -10,7 +10,7 @@ final class prodottoValidator {
         RequestValidator validator=new RequestValidator(request);
         validator.assertMatch("nome", Pattern.compile("^(\\w|\\s){1,30}$"), "nome compreso tra 1 e 30 caratteri");
         validator.assertMatch("ingredienti", Pattern.compile("^(\\w|\\s|,){2,100}$"), "ingredienti compreso tra 2 e 100 caratteri");
-        validator.assertMatch("info", Pattern.compile("^(\\w|\\s){1,50}$"), "info compreso tra 1 e 50 caratteri");
+        validator.assertMatch("info", Pattern.compile("^(\\w|\\s){1,100}$"), "info compreso tra 1 e 50 caratteri");
         validator.assertPositiveDouble("prezzo","prezzo deve essere un numero con la virgola");
         validator.assertMatch("sconto", Pattern.compile("^((100)|[0-9]?[0-9]?)$"),"sconto deve essere un intero tra 0 e 100");
         validator.assertMatch("tipologia", Pattern.compile("^\\w{2,20}$"), "tipologia compreso tra 2 e 20 catteri");
