a5e97597edffadd4a3fb55d73b825708fa10cf99
Daniele <d.donia@studenti.unisa.it>
2021-07-13 01:24:00 +0200

    modificato

--- src/main/webapp/js/dis_validation.js
+++ src/main/webapp/js/dis_validation.js
@@ -13,7 +13,7 @@ form.addEventListener('submit', function (event) {
             event.preventDefault();
         }
 
-        if(ap>=ch){
+        if(chiuso==false && ap>=ch){
             alert(giorni[i] + ": ora Apertura deve essere precedente ad ora Chiusura");
             event.preventDefault();
         }
